52f87ee76ea8351ad65a9b69b6506e12af3792e3
Colt McCormick <coltmccormick@hotmail.com>
2018-03-01 19:16:30 -0600

    completed challenge 1

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
-console.log('hello world!')
+//main.js
+
+//console.log('hello world!')
+
+//Challenge #1
+
+var challengeOneInput = "jumbo shrimp"; 
+ console.log("challengeOneInput: ", challengeOneInput);//display challengeOneInput
+
+var challengeOneInput = challengeOneInput.split(" ");   
+var alphabet = "abcdefghijklmnopqrstuvwxyz"; //set alphabet var
+var firstWordArray = challengeOneInput[0].split(''); //splits first word
+var secondWordArray = challengeOneInput[1].split(''); //splits second word 
+var firstWordCount = 0; 
+var secondWordCount = 0; 
+var challenge1HTML = document.getElementById('challenge1'); 
+
+for (i=0; i<firstWordArray.length; i++){
+    firstWordCount += alphabet.indexOf(firstWordArray[i]) + 1; 
+    console.log("firstWordCount: ", firstWordCount); 
+}
+
+for (i=0; i<secondWordArray.length; i++){
+    secondWordCount += alphabet.indexOf(secondWordArray[i]) + 1; 
+    console.log("secondWordCount: ", secondWordCount); 
+}
+
+if(firstWordCount > secondWordCount){
+        challenge1HTML.innerHTML = "<h3>" + firstWordArray.join('') + "</h3>"
+} else{
+    challenge1HTML.innerHTML = "<h3>" + secondWordArray.join('' ) + "</h3>"
+}
+
+//Challenge #2
