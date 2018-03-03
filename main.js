1ec1d69c8401af1765e040c22576c61c20e7fa2a
Colt McCormick <coltmccormick@hotmail.com>
2018-03-02 22:02:23 -0600

    completed challenge4

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('hello world!')
+//main.js
+
+
+//Challenge #4
+
+var challengeFourInputA = [ 1, 2, 3 ];  // [2, 4, 6]
+var challengeFourInputB = [ 3, 8, 1, 2, 4, 12 ];  // [ 6, 16, 2, 4, 8, 24 ]
+var challenge4Html = document.getElementById("challenge4");
+
+for(m=0;m < challengeFourInputB.length; m++){ //loop through array
+    challengeFourInputB[m] = challengeFourInputB[m]* 2; //multiply index position * 2
+}
+
+challenge4Html.innerHTML = "<h3>" + challengeFourInputB + "</h3>";
