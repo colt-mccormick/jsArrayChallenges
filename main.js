2bd4fb946678eccc320a9f346b0ada9b25aa8347
Colt McCormick <coltmccormick@hotmail.com>
2018-03-03 22:19:40 -0600

    completed challenge 5

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log('hello world!')
+//main.js
+
+//Challenge #5
+
+var array1Challenge5 = [1,2, 4, 7, 5, 9];
+var array2Challenge5 = [5, 9, 2];
+//display = [1, 4, 7]
+
+var challenge5Html= document.getElementById("challenge5")
+var arrayAnswer5=[];
+for(n=0; n < array1Challenge5.length; n++){//run for loop
+   if(array2Challenge5.includes(array1Challenge5[n])){
+   }else{
+    arrayAnswer5.push(array1Challenge5[n]);
+   }
+}
+
+challenge5Html.innerHTML = "<h3>" + arrayAnswer5 + "</h3>";
