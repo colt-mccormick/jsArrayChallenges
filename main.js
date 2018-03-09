0db3127854a8a2a46a49a1c034af1544d4af2cc9
Colt McCormick <coltmccormick@hotmail.com>
2018-03-08 23:02:08 -0600

    completed challenge10

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('hello world!')
+//main.js
+
+//challenge10
+
+var array10 =  [3,6,9];//hide pins 3,5,9
+var challenge10Html = document.getElementById("challenge10");
+
+for (var t = 0; t < array10.length; t++){
+    var pin = '"' +  array10[t] + '"';
+    console.log("pin", pin);
+    var pinHtml = document.getElementById(array10[t]);
+    console.log("t ", array10[t]);  
+    pinHtml.innerHTML = "&nbsp";  
+}
