415a51b0417684eabd46cc159ea1c6c4815617ab
Colt McCormick <coltmccormick@hotmail.com>
2018-03-08 23:10:21 -0600

    removed console logs challenge9

--- main.js
+++ main.js
@@ -10,13 +10,10 @@ var challenge9Html = document.getElementById("challenge9")
 for ( var s = 0; s < phoneNumber.length; s++){
     if(s < 3){
         areaCode.push(phoneNumber[s]);
-        console.log("areacode", areaCode)
     } else if(s >= 3 && s < 6){
         preFix.push(phoneNumber[s]);
-        console.log("prefix",preFix)
     } else{
         lastFour.push(phoneNumber[s])
-        console.log("lastFour", lastFour)
     }
 }
 var phoneDisplay = "(" + areaCode.join("") + ") " + preFix.join("") + "-" + lastFour.join("");
