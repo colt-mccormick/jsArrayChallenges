cad7e61236305c1c6c4e89380f4bc6e19ccf8f85
Colt McCormick <coltmccormick@hotmail.com>
2018-03-08 23:03:32 -0600

    removed console logs challenge 10

--- main.js
+++ main.js
@@ -7,8 +7,6 @@ var challenge10Html = document.getElementById("challenge10");
 
 for (var t = 0; t < array10.length; t++){
     var pin = '"' +  array10[t] + '"';
-    console.log("pin", pin);
-    var pinHtml = document.getElementById(array10[t]);
-    console.log("t ", array10[t]);  
+    var pinHtml = document.getElementById(array10[t]);  
     pinHtml.innerHTML = "&nbsp";  
 }
